8058df175cf793c2378d12ec0925e92a76323cbb
carlos <cgarciapieto@gmail.com>
2019-01-22 12:05:21 -0600

    classwork

---
+++ pythonreviewclasswork.py
@@ -0,0 +1,55 @@
+def main():
+
+
+
+
+# exercise1
+# Python program that prints all the numbers from 0 to 6 except 3 and 6.
+# with an expected output of 1245
+
+#     def exercise1():
+#         number = 0
+#
+#
+# for number in range(6):
+#     number = number + 1
+#
+#     if number == 3:
+#
+#         continue  # continue here
+#
+#     elif number == 6:
+#
+#         continue
+#
+#     print(str(number))
+
+
+# excercise2
+# Python program that counts the number of even and odd numbers from a series of numbers.
+
+# def exercise2():
+#     numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+#     evenCount = 0
+#     oddCount = 0
+#     for x in numbers:
+#         if not x % 2:
+#             evenCount += 1
+#         else:
+#             oddCount += 1
+#     print("Even numbers are :", evenCount)
+#     print("Odd numbers are :", oddCount)
+
+
+    #Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output after User enters a blank line to end.
+        def exercise3():
+            user = input("type a sentence")
+
+            print(user)
+
+
+if __name__ == '__main__':
+
+
+
+    main()
